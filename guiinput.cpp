05c7692d78c05bca17e7300134e5eb6cca86159c
FraserEdwards <fraseredwards@hotmail.co.uk>
2014-03-26 18:45:50 +0000

    input guy functions created

--- guiinput.cpp
+++ guiinput.cpp
@@ -18,27 +18,162 @@ void guiinput::on_Runbutton_clicked()
 
 }
 
-void guiinput::on_checkBox_2_toggled(bool checked)
+void guiinput::on_rangemode_stateChanged(int arg1)
 {
 
 }
 
-void guiinput::on_comboBox_2_currentTextChanged(const QString &arg1)
+void guiinput::on_singlemode_stateChanged(int arg1)
 {
 
 }
 
-void guiinput::on_lineEdit_4_textChanged(const QString &arg1)
+void guiinput::on_parameter_currentTextChanged(const QString &arg1)
 {
 
 }
 
-void guiinput::on_lineEdit_5_textChanged(const QString &arg1)
+void guiinput::on_from_textChanged(const QString &arg1)
 {
 
 }
 
-void guiinput::on_lineEdit_6_textChanged(const QString &arg1)
+void guiinput::on_to_textChanged(const QString &arg1)
+{
+
+}
+
+void guiinput::on_noofpoints_textChanged(const QString &arg1)
+{
+
+}
+
+void guiinput::on_materialname_textChanged(const QString &arg1)
+{
+
+}
+
+void guiinput::on_outsidediameter_textChanged(const QString &arg1)
+{
+
+}
+
+void guiinput::on_density_textChanged(const QString &arg1)
+{
+
+}
+
+void guiinput::on_dynamicmodulus_textChanged(const QString &arg1)
+{
+
+}
+
+void guiinput::on_Tabs_tabBarClicked(int index)
+{
+
+}
+
+void guiinput::on_deltadynamicmodulus_textChanged(const QString &arg1)
+{
+
+}
+
+void guiinput::on_creepmodulus_textChanged(const QString &arg1)
+{
+
+}
+
+void guiinput::on_dynpoissonratio_textChanged(const QString &arg1)
+{
+
+}
+
+void guiinput::on_pipename_textChanged(const QString &arg1)
+{
+
+}
+
+void guiinput::on_sdr_textChanged(const QString &arg1)
+{
+
+}
+
+void guiinput::on_groovedepth_textChanged(const QString &arg1)
+{
+
+}
+
+void guiinput::on_relativediameter_textChanged(const QString &arg1)
+{
+
+}
+
+void guiinput::on_fs_stateChanged(int arg1)
+{
+
+}
+
+void guiinput::on_s4_stateChanged(int arg1)
+{
+
+}
+
+void guiinput::on_backfill_stateChanged(int arg1)
+{
+
+}
+
+void guiinput::on_testtemperature_textChanged(const QString &arg1)
+{
+
+}
+
+void guiinput::on_initialpressure_textChanged(const QString &arg1)
+{
+
+}
+
+void guiinput::on_backfilldepth_textChanged(const QString &arg1)
+{
+
+}
+
+void guiinput::on_backfilldensity_textChanged(const QString &arg1)
+{
+
+}
+
+void guiinput::on_solidfraction_textChanged(const QString &arg1)
+{
+
+}
+
+void guiinput::on_waterfraction_textChanged(const QString &arg1)
+{
+
+}
+
+void guiinput::on_fixedlength_stateChanged(int arg1)
+{
+
+}
+
+void guiinput::on_initiallength_textChanged(const QString &arg1)
+{
+
+}
+
+void guiinput::on_crackspeed_textChanged(const QString &arg1)
+{
+
+}
+
+void guiinput::on_fdnumber_textChanged(const QString &arg1)
+{
+
+}
+
+void guiinput::on_plotcrack_stateChanged(int arg1)
 {
 
 }
--- guiinput.h
+++ guiinput.h
@@ -18,15 +18,70 @@ public:
 private slots:
 
     void on_Runbutton_clicked();
-    void on_checkBox_2_toggled(bool checked);
 
-    void on_comboBox_2_currentTextChanged(const QString &arg1);
+    void on_Tabs_tabBarClicked(int index);
 
-    void on_lineEdit_4_textChanged(const QString &arg1);
+    void on_rangemode_stateChanged(int arg1);
 
-    void on_lineEdit_5_textChanged(const QString &arg1);
+    void on_singlemode_stateChanged(int arg1);
 
-    void on_lineEdit_6_textChanged(const QString &arg1);
+    void on_parameter_currentTextChanged(const QString &arg1);
+
+    void on_from_textChanged(const QString &arg1);
+
+    void on_to_textChanged(const QString &arg1);
+
+    void on_noofpoints_textChanged(const QString &arg1);
+
+    void on_materialname_textChanged(const QString &arg1);
+
+    void on_outsidediameter_textChanged(const QString &arg1);
+
+    void on_density_textChanged(const QString &arg1);
+
+    void on_dynamicmodulus_textChanged(const QString &arg1);
+
+    void on_deltadynamicmodulus_textChanged(const QString &arg1);
+
+    void on_creepmodulus_textChanged(const QString &arg1);
+
+    void on_dynpoissonratio_textChanged(const QString &arg1);
+
+    void on_pipename_textChanged(const QString &arg1);
+
+    void on_sdr_textChanged(const QString &arg1);
+
+    void on_groovedepth_textChanged(const QString &arg1);
+
+    void on_relativediameter_textChanged(const QString &arg1);
+
+    void on_fs_stateChanged(int arg1);
+
+    void on_s4_stateChanged(int arg1);
+
+    void on_backfill_stateChanged(int arg1);
+
+    void on_testtemperature_textChanged(const QString &arg1);
+
+    void on_initialpressure_textChanged(const QString &arg1);
+
+    void on_backfilldepth_textChanged(const QString &arg1);
+
+    void on_backfilldensity_textChanged(const QString &arg1);
+
+    void on_solidfraction_textChanged(const QString &arg1);
+
+    void on_waterfraction_textChanged(const QString &arg1);
+
+    void on_fixedlength_stateChanged(int arg1);
+
+    void on_initiallength_textChanged(const QString &arg1);
+
+    void on_crackspeed_textChanged(const QString &arg1);
+
+    void on_fdnumber_textChanged(const QString &arg1);
+
+    void on_plotcrack_stateChanged(int arg1);
 
 private:
     Ui::guiinput *ui;
