082742d544e235aed5f48194a2cc91f60433ecae
FraserEdwards <fraseredwards@hotmail.co.uk>
2014-05-11 15:24:55 +0100

    Revert "Needs reverting"
    This reverts commit cdc5b39926836f6d92995b1d5f31302e8444520c.

--- Solution.h
+++ Solution.h
@@ -2,7 +2,6 @@
 using namespace std;
 
 #include "FDprofile.h"
-#include "Parameters.h"
 
 #ifndef _SOLUTION_H
 #define _SOLUTION_H
@@ -19,7 +18,7 @@ public:
 	
 	vector<double> zeta;
 	vector<double> vptra;	
-    vector <double> z;
+    vector <vector <double> > zetam;
     vector <vector <double > > vptram;
 
     vector <double> aDotc0;
@@ -40,8 +39,7 @@ public:
 	vector <double> g0;
     vector <double> forplot;
 
-    Solution(Parameters parameters);
-    Solution();
+	Solution();
 	void sprofile(vector<double> zeta, vector<double> vptra, const int l);
     void Tvalues(const double aDotc0s, const double p0bars, const double tempDegCs, const double decompressions, const double alphas, const double ms, const double outflowLengths, const double deltaDStars,
 							const double gS1s, const double gUEs, const double gSbs, const double gKbs, const double g0s, const double gG0s, const double gTotals);
